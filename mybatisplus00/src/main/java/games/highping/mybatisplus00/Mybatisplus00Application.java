b5c8da4cc2663e190822ed71a4776f0d70628ce3
HIGH-PING <80367524+HIGH-PING-X@users.noreply.github.com>
2023-11-16 17:01:38 +0800

    Create Mybatisplus00Application.java

---
+++ mybatisplus00/src/main/java/games/highping/mybatisplus00/Mybatisplus00Application.java
@@ -0,0 +1,15 @@
+package games.highping.mybatisplus00;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@MapperScan("games.highping.mybatisplus00.mapper")
+public class Mybatisplus00Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mybatisplus00Application.class, args);
+    }
+
+}
