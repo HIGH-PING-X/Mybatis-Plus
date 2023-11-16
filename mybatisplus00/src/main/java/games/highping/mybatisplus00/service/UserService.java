aebb456933a4a1304a97b590ff3d813052796595
HIGH-PING <80367524+HIGH-PING-X@users.noreply.github.com>
2023-11-16 17:01:43 +0800

    Create UserService.java

---
+++ mybatisplus00/src/main/java/games/highping/mybatisplus00/service/UserService.java
@@ -0,0 +1,9 @@
+package games.highping.mybatisplus00.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import games.highping.mybatisplus00.bean.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService  extends IService<User> {
+}
