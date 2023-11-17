d2d89e2c9d3a7030f8e13d3b2aad6263d166b93d
HIGH-PING <80367524+HIGH-PING-X@users.noreply.github.com>
2023-11-17 15:11:59 +0800

    Update User.java

--- mybatisplus00/src/main/java/games/highping/mybatisplus00/bean/User.java
+++ mybatisplus00/src/main/java/games/highping/mybatisplus00/bean/User.java
@@ -1,13 +1,16 @@
 package games.highping.mybatisplus00.bean;
 
+import com.baomidou.mybatisplus.annotation.EnumValue;
 import com.baomidou.mybatisplus.annotation.TableLogic;
+import games.highping.mybatisplus00.enums.SexEnum;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Data
 //@TableName("noob_user")    指定表名字
-public class User {
+public class User implements Serializable {
     //TableId(type = IdType.ASSIGN_ID)
     private Long id;
     private String username;
@@ -15,4 +18,6 @@ public class User {
     private LocalDateTime dateRegistered;
     @TableLogic
     private Integer tableLogic; //逻辑删除字段
+    @EnumValue
+    private SexEnum sex;
 }
