e7a3c28f4d1d8e5f0c23c7b5009690df14a79420
HIGH-PING <80367524+HIGH-PING-X@users.noreply.github.com>
2023-11-17 15:12:06 +0800

    Create ProductMapper.java

---
+++ mybatisplus00/src/main/java/games/highping/mybatisplus00/mapper/ProductMapper.java
@@ -0,0 +1,9 @@
+package games.highping.mybatisplus00.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import games.highping.mybatisplus00.bean.Product;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductMapper extends BaseMapper<Product> {
+}
